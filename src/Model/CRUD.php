cd9d919b3f2e9025c799c56edc6b7193274d9d6f
root <root@debianITS>
2019-12-20 12:10:28 +0100

    eliminati unit test per classi inesistenti, pulito CRUD.php dal codice commentato e deprecato

--- src/Model/CRUD.php
+++ src/Model/CRUD.php
@@ -59,42 +59,6 @@ class CRUD
           return false;
         }
     }
-/*
-    function insertNewArticle(string $title, string $subhead, string $body, int $idAuth){
-        try {
-            $sqlQuery = "Insert INTO Article(Title, Subheading, Body, Author, Date) VALUES (:title, :subhead, :body, :idAuth, NOW());";
-            $query = $this->conn->prepare($sqlQuery);
-            $query->execute([':title' => $title,':subhead' => ''.$subhead.'',':body' => ''.$body.'',':idAuth'=> ''.$idAuth.'']);
-            printf("Query avvenuta con successo");
-        } catch (PDOException $e) {
-        print("Errors");
-        die(print_r($e));
-        }
-    }
-
-    function updateArticle(string $title, string $subhead, string $body, int $idAuth, int $id){
-        try {
-            $sqlQuery = "UPDATE Article SET Title =:title, Subheading=:subhead, Body=:body, Date=NOW()) WHERE Author=:idAuth AND id=:id;";
-            $query = $this->conn->prepare($sqlQuery);
-            $query->execute([':title' => $title,':subhead' => ''.$subhead.'',':body' => ''.$body.'',':idAuth'=> ''.$idAuth.'', ':id'=> ''.$id.'']);
-            printf("Query avvenuta con successo");
-        } catch (PDOException $e) {
-        print("Errors");
-        die(print_r($e));
-        }
-    }
-
-    function deleteArticle(int $id){
-        try {
-            $sqlQuery = "DELETE from Article where id=:id;";
-            $query = $this->conn->prepare($sqlQuery);
-            $query->execute([ ':id'=> ''.$id.'']);
-            printf("Query avvenuta con successo");
-        } catch (PDOException $e) {
-        print("Errors");
-        die(print_r($e));
-        }
-    }*/
 
 	
 }
